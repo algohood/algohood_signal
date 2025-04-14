7ffb46ec43d51172e82ee41255eae5691f0e04bd
jijingyuan <jijingyuan@algohood.com>
2025-04-14 09:35:50 +0800

    添加选择器管理器以增强模型管理功能，更新缓存和保留大小的获取方式，修复模型更新逻辑以支持选择特征。

--- algoSignal/algoEngine/modelMgr.py
+++ algoSignal/algoEngine/modelMgr.py
@@ -6,7 +6,7 @@
 """
 import importlib
 from algoUtils.reloadUtil import reload_all
-from algoUtils.baseUtil import ModelBase, Sample
+from algoUtils.baseUtil import ModelBase, Sample, SelectorBase
 from algoUtils.schemaUtil import ModelMgrParam
 from typing import Optional, Dict, List
 from ..algoConfig.profileConfig import profile_stats
@@ -16,8 +16,9 @@ class ModelMgr:
 
     def __init__(self, _model_info: Optional[ModelMgrParam]):
         self.model_mgr = self.get_model_method(_model_info)
-        self.cache_size = _model_info.model_cache_size if _model_info else 100
-        self.retain_size = _model_info.model_retain_size if _model_info else 50
+        self.selector_mgr = self.get_selector_method(_model_info)
+        self.cache_size = _model_info.cache_size if _model_info else 100
+        self.retain_size = _model_info.retain_size if _model_info else 50
         self.cache = []
 
     @staticmethod
@@ -34,6 +35,21 @@ class ModelMgr:
         instance = cls_method(**_method_info.model_method_param)
         return instance
 
+    @staticmethod
+    def get_selector_method(_method_info: Optional[ModelMgrParam]) -> Optional[SelectorBase]:
+        if _method_info is None:
+            return
+
+        module = importlib.import_module('algoStrategy.algoModels.{}'.format(_method_info.selector_method_name))
+        reload_all(module)
+        cls_method = getattr(module, 'Algo')
+        if cls_method is None:
+            raise Exception('Unknown Method: {}'.format(_method_info.selector_method_name))
+        
+        instance = cls_method(**_method_info.selector_method_param)
+        return instance
+
+
     @profile_stats.update_cost_time
     def predict_target(self, _features: Dict[str, float]) -> Optional[Dict[str, float]]:
         if self.model_mgr is None or not _features:
@@ -43,7 +59,7 @@ class ModelMgr:
     
     @profile_stats.update_cost_time
     def update_model(self, _samples: List[Sample]):
-        if not self.model_mgr:
+        if not self.model_mgr or not self.selector_mgr:
             return
         
         features = []
@@ -59,6 +75,10 @@ class ModelMgr:
                         targets.append({field: sample.targets[field] for field in sample.fields})
                     else:
                         targets.append(sample.targets)
+            
+            feature_fields = self.selector_mgr.select_features(features, targets)
+            if feature_fields:
+                features = [{k: v for k, v in feature.items() if k in feature_fields} for feature in features]
 
             if len(features) == len(targets):
                 self.model_mgr.train_model(features, targets)
